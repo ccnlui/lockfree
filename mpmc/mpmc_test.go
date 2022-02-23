d1078c68dd026e89090c99d96949bab469240ce0
calvin <ccnlui@gmail.com>
2022-02-23 14:48:01 -0500

    parallel benchmark mpmc

--- mpmc/mpmc_test.go
+++ mpmc/mpmc_test.go
@@ -1,6 +1,8 @@
 package mpmc
 
-import "testing"
+import (
+	"testing"
+)
 
 func BenchmarkChannel(b *testing.B) {
 	ch := make(chan interface{}, 8192)
@@ -30,4 +32,44 @@ func BenchmarkMPMC(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		q.Put(`a`)
 	}
+}
+
+func BenchmarkChannelConcurrentWrite(b *testing.B) {
+	ch := make(chan interface{}, 8192)
+	// numGr := runtime.GOMAXPROCS(0)
+
+	b.ResetTimer()
+	// 1 Consumer.
+	go func() {
+		for i := 0; i < b.N; i++ {
+			<-ch
+		}
+	}()
+
+	// N Producers.
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			ch <- `a`
+		}
+	})
+}
+
+func BenchmarkMPMCConcurrentWrite(b *testing.B) {
+	q := NewRingBuffer(8192)
+	// numGr := runtime.GOMAXPROCS(0)
+
+	b.ResetTimer()
+	// 1 Consumer.
+	go func() {
+		for i := 0; i < b.N; i++ {
+			q.Get()
+		}
+	}()
+
+	// N Producers.
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			q.Put(`a`)
+		}
+	})
 }
